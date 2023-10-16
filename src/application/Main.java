c37d18db988fd3b63da3ebbea0ec91e5ca64ced5
Jorge Matheus <crossfire_brasil2013@hotmail.com>
2023-10-16 19:16:13 -0300

    testando ainda

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,8 +3,11 @@ package application;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a = 2;
+		int b = 3;
+		
+		System.out.println("Soma " + (a + b));
 	}
 
 }
