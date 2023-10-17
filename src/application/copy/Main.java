f7ad45f41d5b0beb83002152f6339cb15f8976f7
Jorge Matheus <crossfire_brasil2013@hotmail.com>
2023-10-17 18:35:13 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/copy/Main.java
+++ src/application/copy/Main.java
@@ -2,6 +2,8 @@ package application.copy;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,7 @@ public class Main {
 		Department dmp1 = new Department(1, "Book");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, dmp1);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 		
 	}
