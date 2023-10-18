09886580e9d66f02e4d387a591758d803e9dcf75
Jorge Matheus <crossfire_brasil2013@hotmail.com>
2023-10-18 19:44:41 -0300

    delete implementation

--- src/application/copy/Main.java
+++ src/application/copy/Main.java
@@ -2,6 +2,7 @@ package application.copy;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,9 @@ import model.entities.Seller;
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		
 		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
@@ -40,11 +43,23 @@ public class Main {
 		System.out.println("Inserted! new Id = " + newSeller.getId());
 		
 		
-		System.out.println("\n=== TEST 4: seller findByAll =====");
+		System.out.println("\n=== TEST 5: seller findByAll =====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		
+		System.out.println("\n=== TEST 6: seller findByAll =====");
+		
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		
+		System.out.println("Deleted completed");
+		
+		sc.close();
+		
 		
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -93,7 +93,24 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller "
+					+ "WHERE Id = ?");
+			st.setInt(1, id);
+			
+			int rows = st.executeUpdate();
+			
+			if(rows == 0) {
+				throw new DbException("Not possible realize the delete, Don't have Id");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
